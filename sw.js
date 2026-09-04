f2cd5310c9372e679ef6fc7ba76724248cf0f6db
elijahmanlockedin112 <elijahmanlockedin112@users.noreply.github.com>
2026-09-04 07:08:55 -0400

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'c-mtmubchm';
-const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './app.enc'];
+const CACHE = 'c-mtmupkjm';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './app.enc'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
