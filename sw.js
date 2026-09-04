036520c7a05e0affd79fccf47ec81d57e49554d6
elijahmanlockedin112 <elijahmanlockedin112@users.noreply.github.com>
2026-09-04 19:46:06 -0400

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'c-mtnky08i';
+const CACHE = 'c-mtnlpsmt';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './app.enc'];
 
 self.addEventListener('install', (e) => {
