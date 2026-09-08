279faa19807a842eedc91723e8fd6f1cdd78c44b
elijahmanlockedin112 <elijahmanlockedin112@users.noreply.github.com>
2026-09-07 21:39:27 -0400

    A finished group of work says so instead of 'about 0 minutes'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'c-mtrzxn0j';
+const CACHE = 'c-mts04n7o';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './app.enc'];
 
 self.addEventListener('install', (e) => {
