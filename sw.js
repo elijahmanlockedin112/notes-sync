d6bf5ea23872c943682f4e028ae444dc1542a87a
elijahmanlockedin112 <elijahmanlockedin112@users.noreply.github.com>
2026-09-21 06:50:53 -0400

    Update bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'c-mu8ngium';
+const CACHE = 'c-mub4jrel';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './app.enc'];
 
 self.addEventListener('install', (e) => {
